a187fc04247439dbdac243956e9954ef31041461
Aniket Kumar Sharma <ak5154639@gmail.com>
2026-09-10 00:28:02 +0530

    removed e2e test

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 const middleware = require('./utils/middleware')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
@@ -9,7 +10,7 @@ const app = express()
 
 
 
-app.use(express.static('dist'))
+app.use(express.static(path.join(__dirname, 'dist')))
 app.use(express.json())
 app.use(middleware.getTokenFrom)
 app.use(middleware.requestLogger)
@@ -22,6 +23,10 @@ if (process.env.NODE_ENV === 'test') {
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
+
+app.get(/^(?!\/api(?:\/|$)).*/, (request, response) => {
+  response.sendFile(path.join(__dirname, 'dist', 'index.html'))
+})
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
